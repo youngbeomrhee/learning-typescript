591bc3527e7215aeebb023af6ac563c67395910c
이영범 <youngbeomrhee@zigbang.com>
2023-11-08 17:54:32 +0900

    feat: Ch 6

---
+++ ch006-array/lists.ts
@@ -0,0 +1,10 @@
+{
+  function logTrio(name: string, value: [number, boolean]) {
+    console.log(name, value[0], value[1])
+  }
+  const trios: [string, [number, boolean]][] = [
+    ['a', [1, true]],
+    ['b', [2, true]],
+    ['c', [3, true]],
+  ]
+}
